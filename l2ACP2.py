e6df4d4d8890d1e7424b706707fe5d155db82616
angel-kato <angel.codingal@gmail.com>
2026-02-19 15:29:05 +0300

    Add files via upload

---
+++ l2ACP2.py
@@ -0,0 +1,14 @@
+# Store birthdays in 5 different variables
+birthday1 = "15-01-2010"  # Example: 15th Jan 2010
+birthday2 = "22-03-2011"
+birthday3 = "05-06-2012"
+birthday4 = "18-09-2013"
+birthday5 = "30-12-2014"
+
+# Print them to check
+print("Here are the birthdays:")
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
