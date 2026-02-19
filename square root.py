9d19329150af66b25a499571eec845a792104951
angel-kato <angel.codingal@gmail.com>
2026-02-19 16:02:10 +0300

    Add files via upload

---
+++ square root.py
@@ -0,0 +1,10 @@
+import math
+
+# Ask user for a number
+number = float(input("Enter a number: "))
+
+# Calculate square root
+square_root = math.sqrt(number)
+
+# Display result
+print("The square root of", number, "is", square_root)
